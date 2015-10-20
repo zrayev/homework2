466fdb1db6f19eca22009c27068b0291ec74b7e5
dmitry <zrayev@mail.ru>
2015-10-21 02:07:26 +0300

    new work

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
-<form method="post" ></form>
-A <input type="text" name="a"  title=""/> <br>
-<select name="task" title="">
-    <option value="+">+</option>
-    <option value="-">-</option>
-    <option value="*">*</option>
-    <option value="/">/</option>
-</select> <br>
-B <label>
-    <input type="text" name="b"/>
-</label> <br>
-<input type="submit" name="submit" value="Розрахувати" /> <br>
+<form action="" method="post">
+    <input type="text" name="a" title=""/> <br>
+    <select name="task" title="">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select> <br>
+    <label>
+        <input type="text" name="b"/>
+    </label> <br>
+    <input type="submit" name="submit" value="="/> <br>
+</form>
+
 
 <?php
 if (isset($_POST['submit'])) {
@@ -17,9 +19,9 @@ if (isset($_POST['submit'])) {
         $result = 'Число А не заповнене';
     } elseif (empty($_POST['b'])) {
         $result = 'Число В не заповнене';
-    } elseif (!preg_match("/^([0-9\-])+$/", $_POST['a'])) {
+    } elseif (!preg_match("|^[-0-9\.A-F]{1,12}$|i", $_POST['a'])) {
         $result = 'Введіть в поле А цифру';
-    } elseif (!preg_match("/^([0-9\-])+$/", $_POST['b'])) {
+    } elseif (!preg_match("|^[-0-9\.A-F]{1,12}$|i", $_POST['b'])) {
         $result = 'Введіть в поле В цифру';
     } else {
         $a = $_POST['a'];
@@ -37,5 +39,5 @@ if (isset($_POST['submit'])) {
         }
     }
 }
-    echo 'Результат: '. $result;
+echo 'Результат: ' . $result;
 
